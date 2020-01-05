8fc32864f7986460eb8234d08103a86dae20b68a
hajdu-kis.daniel <hajdu-kis.daniel@microsec.hu>
2020-01-05 13:45:39 +0100

    Initial commit

---
+++ src/main/kotlin/hu/dhajdukis/springasync/SpringAsyncApplication.kt
@@ -0,0 +1,11 @@
+package hu.dhajdukis.springasync
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAsyncApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAsyncApplication>(*args)
+}
---
+++ src/test/kotlin/hu/dhajdukis/springasync/SpringAsyncApplicationTests.kt
@@ -0,0 +1,12 @@
+package hu.dhajdukis.springasync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAsyncApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
